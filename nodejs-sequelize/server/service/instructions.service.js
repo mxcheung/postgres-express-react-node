0f704b3a256d424005204718ed1579ae10b6be4e
max <maxcheung@optusnet.com.au>
2025-02-16 16:24:42 +1100

    add post query filter

--- nodejs-sequelize/server/service/instructions.service.js
+++ nodejs-sequelize/server/service/instructions.service.js
@@ -8,6 +8,19 @@ const findALL = (query, replacements) => {
      });
 };
 
+
+const getInstructionQeury = (param1, param2) => {
+    const query = new InstructionsQuery(param1, param2);
+    if (param1 == 'xyz') {
+        // Filtering JSON field directly in SQL 
+        const accountQueryStatement = `instruction_content->>'Account' in (${accounts.map(a => `'${a}'`).join(',')})`
+        query.addWheres([accountQueryStatement]);
+        return query.getQuery()
+    }  else {
+        return query.getQuery()
+    }
+};
+
 module.exports = {
   findAll,
 };
